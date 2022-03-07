5242c372fad21ea85630ffbf83692aa94c380f8a
Erik Kokinda <erikkokinda@MacBook-Pro-uzivatela-Erik.local>
2022-03-08 00:28:23 +0100

    experiment with sidenav

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 function toggleMobileMenu(menu) {
     menu.classList.toggle('open');
 }
+
+function openNav() {
+    document.getElementByClass("mobile-menu").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  }
+  
+  function closeNav() {
+    document.getElementByClass("mobile-menu").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.body.style.backgroundColor = "white";
+  }
